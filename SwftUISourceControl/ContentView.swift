467a82220f2fda3cbfc22a9dc31988e7e947c0e8
Saydulayev <saydulayev.wien@gmail.com>
2025-01-28 10:08:06 +0100

    Add click me button

--- SwftUISourceControl/ContentView.swift
+++ SwftUISourceControl/ContentView.swift
@@ -14,6 +14,10 @@ struct ContentView: View {
                 .font(.largeTitle)
                 .foregroundStyle(.tint)
             Text("SwiftUI Source Control")
+            
+            Button("Click me") {
+            
+            }
         }
         .padding()
     }
