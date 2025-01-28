8ebefe3e38e4964c9ba08d42f6ba7074b1a2f233
Saydulayev <saydulayev.wien@gmail.com>
2025-01-28 10:11:32 +0100

    Add HomeView

---
+++ SwftUISourceControl/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  SwftUISourceControl
+//
+//  Created by Saydulayev on 28.01.25.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Screen 2")
+    }
+}
+
+#Preview {
+    HomeView()
+}
