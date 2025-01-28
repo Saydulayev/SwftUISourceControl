65aed219fa8f5f669e2a4f7f32bee974064d6363
Saydulayev <saydulayev.wien@gmail.com>
2025-01-28 11:11:14 +0100

    [Feature] Add Hello world text

--- SwftUISourceControl/HomeView.swift
+++ SwftUISourceControl/HomeView.swift
@@ -8,14 +8,14 @@
 import SwiftUI
 
 struct HomeView: View {
-    @State private var title: String = ""
+    @State private var title: String = "Hello World!"
     var body: some View {
         VStack {
             Text("Hi")
             Text("Screen 2")
             
             Text("Screen 2")
-            Text("Screen 2")
+            Text("Screen 3")
         }
     }
 }
