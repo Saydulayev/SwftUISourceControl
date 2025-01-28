6c72606139f2b576721eedb59016409837144bfc
Saydulayev <saydulayev.wien@gmail.com>
2025-01-28 10:26:51 +0100

    [Clean] Add commit message documentation

--- SwftUISourceControl/ContentView.swift
+++ SwftUISourceControl/ContentView.swift
@@ -5,6 +5,26 @@
 //  Created by Saydulayev on 28.01.25.
 //
 
+/*
+COMMIT MESSAGES
+
+NEW FEATURE:
+[Feature] Description of the feature
+
+BUG IN PRODUCTION:
+[Patch] Description of patch
+
+BUG NOT IN PRODUCTION:
+[Bug] Description of the bug
+
+MUNDANE TASKS:
+[Clean] Description of changes
+
+RELEASE:
+[Release] Description of release
+
+*/
+
 import SwiftUI
 
 struct ContentView: View {
@@ -18,7 +38,6 @@ struct ContentView: View {
             Button("Click me") {
             
             }
-            .background(.red)
             .background(.green)
         }
         .padding()
