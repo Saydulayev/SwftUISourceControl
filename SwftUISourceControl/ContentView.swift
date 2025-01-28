4f75350c623cdd85d21072b16cb5339b371edf7e
Saydulayev <saydulayev.wien@gmail.com>
2025-01-28 09:23:33 +0100

    Initial Commit

---
+++ SwftUISourceControl/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwftUISourceControl
+//
+//  Created by Saydulayev on 28.01.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwftUISourceControl/SwftUISourceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwftUISourceControlApp.swift
+//  SwftUISourceControl
+//
+//  Created by Saydulayev on 28.01.25.
+//
+
+import SwiftUI
+
+@main
+struct SwftUISourceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
