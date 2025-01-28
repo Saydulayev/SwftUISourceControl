69d01c83b32d7ea459e0c2b6e6b089bbf03d701d
Saydulayev <saydulayev.wien@gmail.com>
2025-01-28 11:21:44 +0100

    [Clean] Updates to doc.

--- SwftUISourceControl/ContentView.swift
+++ SwftUISourceControl/ContentView.swift
@@ -6,23 +6,30 @@
 //
 
 /*
-COMMIT MESSAGES
-
-NEW FEATURE:
-[Feature] Description of the feature
+ Clone = Copying the repo locally
+ Commit = Save ("checkpoint") on our current branch
+ Stage = Prepare changes for a commit
+ Stash = Save changes for later
+ Push = Send local commits to remote repo
+ Pull = Fetch remote commits to local repo
  
-BUG NOT IN PRODUCTION:
-[Bug] Description of the bug
+ COMMIT MESSAGES
  
-RELEASE:
-[Release] Description of release
-
-BUG IN PRODUCTION:
-[Patch] Description of patch
-
-MUNDANE TASKS:
-[Clean] Description of changes
-*/
+ NEW FEATURE:
+ [Feature] Description of the feature
+ 
+ BUG NOT IN PRODUCTION:
+ [Bug] Description of the bug
+ 
+ RELEASE:
+ [Release] Description of release
+ 
+ BUG IN PRODUCTION:
+ [Patch] Description of patch
+ 
+ MUNDANE TASKS:
+ [Clean] Description of changes
+ */
 
 import SwiftUI
 
