077f46144aab19b057ab849f8eb81785ac90ebab
Saydulayev <saydulayev.wien@gmail.com>
2025-01-29 14:01:11 +0100

    [Feature] Fix   one

--- SwftUISourceControl/ContentView.swift
+++ SwftUISourceControl/ContentView.swift
@@ -42,7 +42,7 @@ struct ContentView: View {
                         Image(systemName: "magnifyingglass")
                             .font(.largeTitle)
                             .foregroundStyle(.tint)
-                        Text("Some new title")
+                        Text("Some new title!!!")
                         
                         Button("Swiftui Source Control") {
                             
