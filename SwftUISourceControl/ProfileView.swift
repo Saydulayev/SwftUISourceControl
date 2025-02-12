a10729a6a4418838736e69978365590eb5c239ac
Saydulayev <saydulayev.wien@gmail.com>
2025-02-12 10:53:56 +0100

    [Feature] Add ProfileView

---
+++ SwftUISourceControl/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  SwftUISourceControl
+//
+//  Created by Saydulayev on 12.02.25.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text("Hello, SwiftUI!")
+    }
+}
+
+#Preview {
+    ProfileView()
+}
